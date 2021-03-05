17b1e229e7d7ae94bba8dcb12813fb960ab289a4
Stephen Senft <stephensenft21@gmail.com>
2021-03-05 15:32:04 -0600

    deleted the hello world component and added two new files

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+ <h2>{{ todo.name }}</h2>
+ <button @click="handleClick"> Mark Completed</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["todo"],
+
+  methods: {
+      handleClick() {
+          this.$$emit("status-change", this.todo)
+      }
+  }
+};
+</script>
+
+<style scoped>
+
+div {
+  border: 3px solid tomato;
+  margin: 15px;
+  padding: 10px;
+}</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>ToDo List</h3>
+    <div v-for="item in todos" :key="item.id">
+      <todo-item :todo="item" @status-change="handleStatusChange" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { todoItems } from "";
+import TodoItem from "./TodoItem";
+
+export default {
+  components: { TodoItem },
+
+  data() {
+    return {
+      todos: [...todoItems],
+    };
+  },
+  methods: {
+    handleStatusChange(item) {
+      item.complete = !item.complete;
+      console.log(item);
+    },
+  },
+};
+</script>
+
+<style></style>
