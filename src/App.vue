f4249292760eb37a1a854ef741cc95755cd5dcec
Stephen Senft <stephensenft21@gmail.com>
2021-03-12 12:04:59 -0600

    todo list complete

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-   <todo-list />
+<todo-list/>
   </div>
 </template>
 
 <script>
 import TodoList from "./components/TodoList";
 
+
 export default {
-  components: {
-    TodoList,
-  }
+   components: {
+    TodoList
+  },
 }
 </script>
 
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>ToDo List</h3>
+    <h5>There are {{ itemsRemaining}} items left to do today</h5>
     <div v-for="item in todos" :key="item.id">
       <todo-item :todo="item" @status-change="handleStatusChange" />
     </div>
@@ -8,12 +9,10 @@
 </template>
 
 <script>
-import { todoItems } from "";
+import { todoItems } from "../data";
 import TodoItem from "./TodoItem";
-
 export default {
   components: { TodoItem },
-
   data() {
     return {
       todos: [...todoItems],
@@ -25,7 +24,15 @@ export default {
       console.log(item);
     },
   },
+
+  computed: {
+    itemsRemaining() {
+      return this.todos.filter((t) => !t.complete).length;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+
+</style>
