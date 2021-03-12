a015cfec7a1f9d3d12eeacefc95208c67bda7d7f
Stephen Senft <stephensenft21@gmail.com>
2021-03-12 13:42:02 -0600

    adding vuetify library to to do app

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h3>ToDo List</h3>
-    <h5>There are {{ itemsRemaining}} items left to do today</h5>
+    
+    <h5 v-if="itemsRemaining > 1 ">There are {{ itemsRemaining}} items left to do today</h5>
+    <h5 v-else-if="itemsRemaining < 2 && itemsRemaining === 1">There is {{ itemsRemaining}} item left to do today</h5>
+    <h5 v-else-if="itemsRemaining === 0 ">No More items left to do today!!!</h5>
     <div v-for="item in todos" :key="item.id">
       <todo-item :todo="item" @status-change="handleStatusChange" />
     </div>
@@ -9,6 +12,7 @@
 </template>
 
 <script>
+
 import { todoItems } from "../data";
 import TodoItem from "./TodoItem";
 export default {
@@ -27,7 +31,10 @@ export default {
 
   computed: {
     itemsRemaining() {
-      return this.todos.filter((t) => !t.complete).length;
+      const filteredItems =  this.todos.filter((t) => !t.complete).length;
+      console.log(filteredItems)
+    return filteredItems
+    
     },
   },
 };
